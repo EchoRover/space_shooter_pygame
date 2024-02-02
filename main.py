5cb6c243909ce42a14fa1c91311a16354c57e150
Evan Tobias <105860929+EchoRover@users.noreply.github.com>
2024-02-02 23:33:47 +0400

    added player,background,bullets

---
+++ main.py
@@ -0,0 +1,178 @@
+import pygame
+import os
+import random
+
+S_WIDTH ,S_HEIGHT = 768,591
+pygame.init()
+pygame.display.set_caption("Generic Space Shooter GSS")
+icon = pygame.image.load("graphics/icon.png")
+pygame.display.set_icon(icon)
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode((S_WIDTH,S_HEIGHT))
+
+
+
+class Player(pygame.sprite.Sprite):
+
+    def __init__(self):
+        super().__init__()
+        self.x = S_WIDTH//2 
+        self.y = S_HEIGHT - 50
+        self.image = pygame.image.load("graphics/player/ship_I.png").convert_alpha()
+        
+        self.orginal = self.image.copy()
+        self.image = pygame.transform.scale_by(self.orginal,0.7)
+        self.rect = self.image.get_rect(center = (self.x,self.y))
+
+        self.acceleration = 2
+        self.resistance = 0.6
+        
+        self.direction = pygame.math.Vector2()
+
+        self.vx = self.vy = 0
+        self.pull = 0
+
+        self.shoot_cooldown = 0.3
+        self.last_shot_time = pygame.time.get_ticks()
+    
+    
+    def inputs(self):
+        keys = pygame.key.get_pressed()
+        self.direction.y = -(keys[pygame.K_UP] or keys[pygame.K_w]) + (keys[pygame.K_DOWN] or keys[pygame.K_s])
+        self.direction.x = (keys[pygame.K_RIGHT] or keys[pygame.K_d]) - (keys[pygame.K_LEFT] or keys[pygame.K_a])
+
+        current_time = pygame.time.get_ticks()
+
+        
+        if (keys[pygame.K_SPACE] or keys[pygame.MOUSEBUTTONDOWN]) and current_time - self.last_shot_time > self.shoot_cooldown * 1000:
+            bullets_group.add(Bullets(self.rect.centerx, self.rect.centery))
+            self.last_shot_time = current_time  
+
+    
+    def update(self):
+        self.inputs()
+        if self.direction.length() != 0:
+            self.direction.normalize_ip()
+        self.vx = (self.resistance * self.vx) + (self.acceleration * self.direction.x)
+        self.vy = (self.resistance * self.vy) + (self.acceleration * self.direction.y)  
+        # self.vx = 5 * self.direction.x      
+        # self.vy = 5 * self.direction.y      
+
+        self.rect.x += round(self.vx,3)
+        self.rect.y += round(self.vy,3)
+        if self.direction.y < 0:
+            self.pull = (self.pull * 0.3) + 0.01
+        else:
+            self.pull = 0
+
+        self.image = pygame.transform.scale_by(self.orginal,0.7 + round(self.vy,2) * 0.007)
+        self.image = pygame.transform.rotozoom( self.image, -self.vx,0.9 )
+
+
+class Bullets(pygame.sprite.Sprite):
+	def __init__(self,xx,yy):
+		super().__init__()
+		self.image = pygame.image.load("graphics/bullet.png").convert_alpha()
+		self.rect = self.image.get_rect(center = (xx,yy))
+		self.speed = 0
+	
+	def update(self):
+		self.speed = (self.speed * 0.8) + 2
+		self.rect.y -= self.speed
+		self.outscreenkill()
+	
+	def outscreenkill(self):
+		if self.rect.y < -30:
+			self.kill()
+
+
+class Background(pygame.sprite.Sprite):
+    def __init__(self,x = None, y = None):
+        super().__init__()
+        weights = [7,10,1,2,1,1,1]
+        myimg = os.listdir("graphics/background")
+        assert len(weights) == len(myimg) ,"error image weight background"
+        new = []
+        for i in range(len(weights)):
+            new.extend([myimg[i]] * weights[i])
+        self.myimg = random.choice(new)
+
+       
+        self.image = pygame.image.load("graphics/background/" + self.myimg).convert_alpha()
+    
+     
+        self.image = pygame.transform.scale_by(self.image,random.randint(30,80) * 0.01)
+
+        if "station" in self.myimg:
+            self.states = [pygame.transform.rotate(self.image,i) for i in range(1,360,random.randint(1,3))]
+            if random.randint(0,1) == 1:
+                self.states.reverse()
+
+        if x == None or y == None:
+            self.rect = self.image.get_rect(center = (random.randint(0,S_WIDTH),random.randint(-500,-10)))
+        else:
+            self.rect = self.image.get_rect(center = (x,y))
+
+        self.speed = random.randint(10,15)/10
+
+        self.count = 0
+        
+    
+    def update(self):
+        if "station" in self.myimg:
+            self.image = self.states[self.count]
+            self.rect = self.image.get_rect(center = self.rect.center)
+
+            self.count  = int((self.count + 1) % len(self.states))
+
+  
+        self.rect.y += self.speed + player.pull
+        self.remove()
+
+    
+    def remove(self):
+        if self.rect.y > S_HEIGHT + 200:
+            self.kill()
+
+
+
+player = Player()
+player_group = pygame.sprite.Group()
+player_group.add(player)
+
+bullets_group = pygame.sprite.Group()
+
+background_group = pygame.sprite.Group()
+for i in range(20):
+    background_group.add(Background(random.randint(0,S_WIDTH),random.randint(-50,S_HEIGHT + 20)))
+
+while True:
+
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+        # elif event.type == pygame.KEYDOWN:
+        #     player.update()
+        
+    # player_group.update()
+    if len(background_group) < 30:
+        background_group.add(Background())
+    screen.fill((0,0,0))
+
+    background_group.draw(screen)
+    background_group.update()
+
+    player_group.draw(screen)
+    player_group.update()
+    
+    bullets_group.draw(screen)
+    bullets_group.update()
+
+    pygame.display.flip()
+
+    clock.tick(30)
+
+
+
+    
