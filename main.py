c8d8523c4fd4298471a94a10b6be2877747185eb
Evan Tobias <105860929+EchoRover@users.noreply.github.com>
2024-03-13 19:49:41 +0400

    Update main.py

--- main.py
+++ main.py
@@ -284,21 +284,21 @@ while True:
     clock.tick(30)
 
 
-x,y, = 
+# x,y, = 
  
- draw 
- update
- fps = 30
+#  draw 
+#  update
+#  fps = 30
 
 
-vx = (v * 0.1) + 2* input(up - down)
-0.3
-vy = 
+# vx = (v * 0.1) + 2* input(up - down)
+# 0.3
+# vy = 
 
-1 0
-1 0
+# 1 0
+# 1 0
 
-v = 100
-v = 80
-v - 
+# v = 100
+# v = 80
+# v - 
 
